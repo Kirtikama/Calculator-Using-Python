375e8e4eb985a6bba079c34fc4ad7ba803c279ed
Kirti Kamal <139262669+Kirtikama@users.noreply.github.com>
2024-09-03 13:32:04 +0530

    Add files via upload

---
+++ Python Calculator.py
@@ -0,0 +1,18 @@
+print("Calculator")
+num1= int(input("enter number1  "))
+num2= int(input("enter number2  "))
+operator= input("Please enter a operator + - * / % ** //")
+if operator =="+":
+    print("the sum of two number is",num1+num2)
+if operator =="-":
+    print("the sum of two number is",num1-num2)
+if operator == "*":
+    print("the sum of two number is", num1*num2)
+if operator == "/":
+    print("the sum of two number is", num1/num2)
+if operator == "%":
+    print("the sum of two number is", num1%num2)
+if operator == "**":
+    print("the sum of two number is", num1**num2)
+if operator == "//":
+    print("the sum of two number is", num1//num2)
